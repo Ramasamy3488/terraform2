2b2a3e4a698387bdc5fe6ec46dc26582e93a8265
Ramasamy3488@gmail.com <kvpramasamy@gmail.com>
2024-06-07 10:31:27 +0530

    first commit

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami             = "ami-0c42696027a8ede58"
   instance_type   = "t2.micro"
-  key_name        = "oct3"
+  key_name        = "devopskey"
   security_groups = ["linux-sg"]
 #  count = 2
 
